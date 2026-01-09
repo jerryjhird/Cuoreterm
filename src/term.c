49b211619d4bb286e34efc758b2bc9dedf4b065d
Jozef Nagy <schkwve@gmail.com>
2026-01-09 20:26:18 +0100

    ok now

--- src/term.c
+++ src/term.c
@@ -193,6 +193,7 @@ void handle_ansi(struct terminal * term, char **p_c, int *i)
         return;
     }
     where = *c++ - 0x30;
+    *i++;
 
     if (*c >= '0' && *c <= '9') {
         col += *c++ - 0x30;
@@ -211,6 +212,9 @@ void handle_ansi(struct terminal * term, char **p_c, int *i)
     // this technically allows '\x1b[30;30;30;30;30;30;30;30;30m' but whatever
     if (*c == ';') {
         handle_ansi(term, p_c, i);
+    } else if (*c == 'm') {
+        *p_c++;
+        *i++;
     }
 }
 
